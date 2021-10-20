473601d17fc9f78f807ce9b99c4cea1aa42b5202
unknown <enzo.tapia21@gmail.com>
2021-10-20 09:24:24 -0500

    add logs and add change application.properties to application.yml

--- src/main/java/com/me/pe/currencyexchange/controller/CurrencyExchangeController.java
+++ src/main/java/com/me/pe/currencyexchange/controller/CurrencyExchangeController.java
@@ -8,7 +8,9 @@ import com.me.pe.currencyexchange.model.api.update.CurrencyExchangeUpdateRequest
 import com.me.pe.currencyexchange.model.dto.CurrencyExchangeDto;
 import io.reactivex.Completable;
 import io.reactivex.Maybe;
+import io.reactivex.schedulers.Schedulers;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +18,37 @@ import java.math.BigDecimal;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class CurrencyExchangeController {
 
     private final CurrencyExchangeService service;
 
     @GetMapping("/currency-exchange/calculate")
     public Maybe<CurrencyExchangeResponse> calculate(@RequestBody CurrencyExchangeRequest request) {
-        return service.findByOriginCurrencyAndDestinationCurrency(buildCurrencyExchangeDtoRequest(request))
-                .map(currencyExchangeDto -> buildCurrencyExchangeResponse(request, currencyExchangeDto));
+        return Maybe.fromCallable(() -> buildCurrencyExchangeDtoRequest(request))
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe(disposable -> log.info("Endpoint /calculate - Starting"))
+                .flatMap(service::findByOriginCurrencyAndDestinationCurrency)
+                .map(currencyExchangeDto -> buildCurrencyExchangeResponse(request, currencyExchangeDto))
+                .doOnSuccess(currencyExchangeResponse -> log.info("Endpoint /calculate - Completed"));
     }
 
     @PostMapping("/currency-exchange")
     @ResponseStatus(HttpStatus.CREATED)
     public Completable register(@RequestBody CurrencyExchangeRegisterRequest request) {
-        return service.save(buildCurrencyExchangeDtoRequest(request));
+        return service.save(buildCurrencyExchangeDtoRequest(request))
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe(disposable -> log.info("Endpoint /currency-exchange POST - Starting"))
+                .doOnComplete(() -> log.info("Endpoint /currency-exchange POST - Completed"));
     }
 
     @PatchMapping("/currency-exchange/{exchangeCurrencyId}")
     public Completable update(@PathVariable String exchangeCurrencyId,
                               @RequestBody CurrencyExchangeUpdateRequest request) {
-        return service.update(buildCurrencyExchangeDtoRequestToUpdate(Integer.parseInt(exchangeCurrencyId), request));
+        return service.update(buildCurrencyExchangeDtoRequestToUpdate(Integer.parseInt(exchangeCurrencyId), request))
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe(disposable -> log.info("Endpoint /currency-exchange PATCH - Starting"))
+                .doOnComplete(() -> log.info("Endpoint /currency-exchange PATCH - Completed"));
     }
 
     private CurrencyExchangeDto buildCurrencyExchangeDtoRequest(CurrencyExchangeRequest request) {
--- src/main/java/com/me/pe/currencyexchange/dao/impl/CurrencyExchangeDaoImpl.java
+++ src/main/java/com/me/pe/currencyexchange/dao/impl/CurrencyExchangeDaoImpl.java
@@ -13,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @Component
 @Slf4j
@@ -22,30 +24,56 @@ public class CurrencyExchangeDaoImpl implements CurrencyExchangeDao {
 
     @Override
     public Maybe<CurrencyExchangeDto> findByOriginCurrencyAndDestinationCurrency(CurrencyExchangeDto currencyExchange) {
+
+        String originCurrency = currencyExchange.getOriginCurrency();
+        String destinationCurrency = currencyExchange.getDestinationCurrency();
+
         return Maybe.fromCallable(() ->
-                repository.findByOriginCurrencyAndDestinationCurrency(
-                        currencyExchange.getOriginCurrency(), currencyExchange.getDestinationCurrency()))
+                repository.findByOriginCurrencyAndDestinationCurrency(originCurrency, destinationCurrency))
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe(disposable -> log.debug("Searching currency exchange"))
+                .doOnSubscribe(disposable ->
+                        log.debug("Getting Currency Exchange for [currencyOrigin={}, destinationCurrency={}] starts",
+                                originCurrency, destinationCurrency))
                 .onErrorResumeNext(Maybe.error(buildResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR)))
                 .doOnError(throwable -> log.error(throwable.toString()))
                 .switchIfEmpty(Maybe.error(buildResponseStatusException(HttpStatus.NOT_FOUND)))
+                .doOnError(throwable -> log.error("Error getting Currency Exchange. Not found resource"))
                 .map(this::buildCurrencyExchangeDto)
-                .doOnSuccess(currencyExchangeDto -> log.debug("Found currency exchange"));
+                .doOnSuccess(currencyExchangeDto ->
+                        log.debug("Getting Currency Exchange for [currencyOrigin={}, destinationCurrency={}] completed",
+                                originCurrency, destinationCurrency));
     }
 
     @Override
     public Completable save(CurrencyExchangeDto currencyExchange) {
+        String originCurrency = currencyExchange.getOriginCurrency();
+        String destinationCurrency = currencyExchange.getDestinationCurrency();
+        BigDecimal exchangeRate = currencyExchange.getExchangeRate();
+
         return Completable.fromCallable(() -> repository.save(buildCurrencyExchange(currencyExchange)))
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe(disposable -> log.debug("Save is starting"));
+                .doOnSubscribe(disposable ->
+                        log.debug("Save Currency Exchange for [currencyOrigin={}, destinationCurrency={}, exchangeRate={}] starts",
+                                originCurrency, destinationCurrency, exchangeRate))
+                .doOnComplete(() ->
+                        log.debug("Save Currency Exchange for [currencyOrigin={}, destinationCurrency={}, exchangeRate={}] completed",
+                        originCurrency, destinationCurrency, exchangeRate))
+                .doOnError(throwable -> log.error("Error save currency exchange in H2"));
     }
 
     @Override
     public Completable update(CurrencyExchangeDto currencyExchange) {
+        int id = currencyExchange.getId();
+        BigDecimal exchangeRate = currencyExchange.getExchangeRate();
         return Completable.fromCallable(() -> repository.update(currencyExchange.getId(), currencyExchange.getExchangeRate()))
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe(disposable -> log.debug("Update is starting"));
+                .doOnSubscribe(disposable ->
+                        log.debug("Update Currency Exchange for [idCurrencyExchange={}, exchangeRate={}] starts",
+                                id, exchangeRate))
+                .doOnComplete(() ->
+                        log.debug("Update Currency Exchange for [idCurrencyExchange={}, exchangeRate={}] completed",
+                                id, exchangeRate))
+                .doOnError(throwable -> log.error("Error update currency exchange in H2"));
     }
 
 
